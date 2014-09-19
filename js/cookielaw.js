11a22fdeb88412db1901bdc7d9e667ea40a4dd30
Nicolas Escoffier <nico.oxydesign@free.fr>
2014-09-19 18:24:40 +0200

    Switch from private functions to public methods

--- js/cookielaw.js
+++ js/cookielaw.js
@@ -14,62 +14,65 @@
       // Dom elements
       this.$closeBtn = this.$cookieLawBlock.find('.btn a');
       // init plugin
-      return this._CookieLaw();
+      return this.cookieLaw();
     };
 
     $[pluginName].prototype = {
-      _CookieLaw: function () {
-        var t = this,
-            cookieTimeLeft = 395; // 11 months from now (The current time)
-        var initEntryPoint = function () {
-          (getCookie('cookielaw')) ? t.$cookieLawBlock.hide(): showHideBlock();;
-        };
-        var showHideBlock = function () {
-          // Hide block & Set cookie
-          t.$closeBtn.click(function (e) {
-            e.preventDefault();
-            t.$cookieLawBlock.addClass('hidden');
-            setCookie('cookielaw', 'accepted', cookieTimeLeft);
-          });
-        };
-        //get cookie
-        var getCookie = function (name) {
-          var start = document.cookie.indexOf(name + "=");
-          var len = start + name.length + 1;
-          if ((!start) && (name != document.cookie.substring(0, name.length))) {
-            return null;
-          }
-          if (start == -1) return null;
-          var end = document.cookie.indexOf(';', len);
-          if (end == -1) end = document.cookie.length;
-          //console.log('The cookie ' + name+ ' is set');
-          return unescape(document.cookie.substring(len, end));
-        };
-
-        //set cookie
-        var setCookie = function (name, value, expires, path, domain, secure) {
-          var today = new Date();
-          today.setTime(today.getTime());
-          if (expires) {
-            expires = expires * 1000 * 60 * 60 * 24;
-          }
-          var expires_date = new Date(today.getTime() + (expires));
-          document.cookie = name + '=' + escape(value) +
-            ((expires) ? ';expires=' + expires_date.toGMTString() : '') + //expires.toGMTString()
-            ((path) ? ';path=' + path : '') +
-            ((domain) ? ';domain=' + domain : '') +
-            ((secure) ? ';secure' : '');
-          };
-
-        //delete cookie
-        var deleteCookie = function (name, path, domain) {
-          if (getCookie(name)) document.cookie = name + '=' +
-            ((path) ? ';path=' + path : '') +
-          ((domain) ? ';domain=' + domain : '') +
-          ';expires=Thu, 01-Jan-1970 00:00:01 GMT';
-        };
+      cookieLaw : function () {
+        var self = this;
+        self.cookieTimeLeft = 395; // 13 months from now (The current time)
         // Entry point
-        initEntryPoint();
+        self.initEntryPoint();
+      },
+      initEntryPoint : function () {
+        var self = this;
+        (self.getCookie('cookielaw')) ? self.$cookieLawBlock.hide(): self.showHideBlock();;
+      },
+      showHideBlock : function () {
+        var self = this;
+        // Hide block & Set cookie
+        self.$closeBtn.click(function (e) {
+          e.preventDefault();
+          self.$cookieLawBlock.addClass('hidden');
+          self.setCookie('cookielaw', 'accepted', self.cookieTimeLeft);
+        });
+      },
+      //get cookie
+      getCookie : function (name) {
+        var start = document.cookie.indexOf(name + "=");
+        var len = start + name.length + 1;
+        if ((!start) && (name != document.cookie.substring(0, name.length))) {
+          return null;
+        }
+        if (start == -1) return null;
+        var end = document.cookie.indexOf(';', len);
+        if (end == -1) end = document.cookie.length;
+        //console.log('The cookie ' + name+ ' is set');
+        return unescape(document.cookie.substring(len, end));
+      },
+
+      //set cookie
+      setCookie : function (name, value, expires, path, domain, secure) {
+        var today = new Date();
+        today.setTime(today.getTime());
+        if (expires) {
+          expires = expires * 1000 * 60 * 60 * 24;
+        }
+        var expires_date = new Date(today.getTime() + (expires));
+        document.cookie = name + '=' + escape(value) +
+          ((expires) ? ';expires=' + expires_date.toGMTString() : '') + //expires.toGMTString()
+          ((path) ? ';path=' + path : '') +
+          ((domain) ? ';domain=' + domain : '') +
+          ((secure) ? ';secure' : '');
+      },
+
+      //delete cookie
+      deleteCookie : function (name, path, domain) {
+        var self = this;
+        if (self.getCookie(name)) document.cookie = name + '=' +
+          ((path) ? ';path=' + path : '') +
+        ((domain) ? ';domain=' + domain : '') +
+        ';expires=Thu, 01-Jan-1970 00:00:01 GMT';
       }
     };
     // Building the plugin
